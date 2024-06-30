808052c4de1d3791ecdc924225c1c96b893e5780
Mohamed Mosad <mohamedmosaad115@gmail.com>
2024-06-30 22:33:30 +0300

    Build Deploy Files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   baseUrl: 'https://mohamed3mosaad-001-site1.gtempurl.com/api/',
   baseServerUrl: 'https://mohamed3mosaad-001-site1.gtempurl.com/',
   hubUrl: 'https://mohamed3mosaad-001-site1.gtempurl.com/hubs/',
-  production: false
+  production: true
 };
 
